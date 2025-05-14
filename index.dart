3f124d9446b53e2707061d521cd3a2234486bd4d
daveeeeee123 <dcadilo@yahoo.com>
2025-05-14 23:31:46 +0800

    Fix int to var and printf to print

--- index.dart
+++ index.dart
@@ -1,19 +1,17 @@
-void main()
-{
-    int age =26; 
-    var name = "Dave Cadilo";
+void main() {
+  int age = 26;
+  var name = "Dave Cadilo";
 
-    print(age);
-    print(name);
+  print(age);
+  print(name);
 
+  var x = ["Dave", "Dianne", "Domique"];
+  var j = [1, 2, 3, 4];
+  int? y;
+  print(x);
+  y = 20;
+  print(x);
+  print(j);
 
-    var x = ["Dave","Dianne","Domique"];
-    int j = [1,2,3,4];
-    int? y;
-    print(x);
-    y = 20;
-    print(x);
-    printf(j);
-
-    //Hello World
+  //Hello World
 }
